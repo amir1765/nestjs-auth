514616d5aed92d0b95db577a461029348e7f9c22
User <amir176500@gmail.com>
2026-04-28 05:11:25 -0700

    feat:helmet shit

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { setCryptoKey } from './common/crypto';
-
+import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  // app.enableCors({
+  //   origin: ['https://yourdomain.com'],
+  //   credentials: true,
+  // });
   // 👇 Only run Swagger when not in production
   if (process.env.NODE_ENV !== 'production') {
+    app.use(
+      helmet({
+        contentSecurityPolicy: false,
+      }),
+    );
     const config = new DocumentBuilder()
       .setTitle('My API')
       .setDescription('The cats API description')
@@ -22,6 +33,7 @@ async function bootstrap() {
   }
   const config = app.get(ConfigService);
   setCryptoKey(config.get<string>('CRYPTO_SECRET')!);
+
   await app.listen(3000);
 }
 bootstrap().catch((error) => {
